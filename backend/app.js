a23c8356a483b3efe01f23ebb4bebef2c66ba2b4
ajmalbinnizam <ajmalnizam369@gmail.com>
2021-05-31 01:23:25 +0530

    created angular c and routing

--- backend/app.js
+++ backend/app.js
@@ -65,14 +65,6 @@ const app = express();
 
 
 
-// Import Routes
-const productsRoute = require('./routes/products');
-const usersRoute = require('./routes/users');
-
-// Use Routes
-app.use('/api/products', productsRoute);
-app.use('/api/users', usersRoute);
-
 
 app.use(cors({
     origin: "*",
@@ -88,5 +80,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+
+// Import Routes
+const productsRoute = require('./routes/products');
+const ordersRoute = require('./routes/orders');
+
+// Use Routes
+app.use('/api/products', productsRoute);
+app.use('/api/orders', ordersRoute);
 
 module.exports = app;
